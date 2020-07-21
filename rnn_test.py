99dfdad27d1bac80a2103fb28245844a24192532
yonghee12 <suncar1000@gmail.com>
2020-07-22 01:53:26 +0900

    Initial Commit

---
+++ rnn_test.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+from rnn_numpy_models import VanillaRNN
+
+input_dim, hidden_dim = 3, 5
+
+x_t = np.random.normal(0, 1, size=(1, input_dim))
+h_prev = np.random.normal(0, 1, size=(1, hidden_dim))
+
+model = VanillaRNN(input_dim, hidden_dim)
+model.forward(x_t, h_prev)
+model.backward(grad_up=1)
