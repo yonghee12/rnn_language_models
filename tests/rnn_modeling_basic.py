4adf0e2e924b6f9ef3027440214a20044e5377b6
yonghee12 <suncar1000@gmail.com>
2020-07-23 22:39:09 +0900

    testing rnn lm training

--- tests/rnn_modeling_basic.py
+++ tests/rnn_modeling_basic.py
@@ -12,19 +12,27 @@ stopwords_agg = stopwords.words('english') + list(punctuation)
 stopwords_hash = {k: 1 for k in stopwords_agg}
 
 
-corpus = """경마장에 있는 말이 뛰고 있다\n
-그의 말이 법이다\n
-가는 말이 고와야 오는 말이 곱다\n"""
+def basic_tokenize(corpus):
+    """
+    Example)
+    corpus = "경마장에 있는 말이 뛰고 있다\n
+    그의 말이 법이다\n
+    가는 말이 고와야 오는 말이 곱다\n"
+    """
+    if isinstance(corpus, str):
+        return [doc.strip() for doc in corpus.split('\n') if doc]
+    elif isinstance(corpus, Iterable):
+        return [doc.strip() for doc in corpus if doc]
 
-docs = [doc.strip() for doc in corpus.split('\n') if doc]
-tokens_matrix = [[token for token in doc.split(' ') if token] for doc in docs]
 
-uniques = get_uniques_from_nested_lists(tokens_matrix)
-token2idx, idx2token = get_item2idx(uniques, unique=True, from_one=True)
+def get_tokenized_matrix(docs: List[str]):
+    return [[token for token in doc.split(' ') if token] for doc in docs]
 
-sequences = []
-for tokens in tokens_matrix:
-    sequences += get_sequences_from_tokens(tokens, token2idx)
+
+
+
+unique_tokens = get_uniques_from_nested_lists(tokens_matrix)
+token2idx, idx2token = get_item2idx(self.unique_tokens, unique=True, from_one=True)
 
 max_len = max([len(arr) for arr in sequences])
 padded_sequences = pad_sequence_nested_lists(sequences, max_len, method='pre', truncating='pre')
@@ -35,11 +43,3 @@ y = seqs[:, -1]
 
 num_classes = len(token2idx) + 1  # 0번 인덱스는 one-hot에서 사용을 못함 (1 ~ 12 이므로)
 y_onehot = to_categorical_iterable(y, num_classes)
-
-# getting sample text
-austen_fileids = [f for f in nltk.corpus.gutenberg.fileids() if f.startswith('austen')]
-austen_fileids[0]
-emma = nltk.corpus.gutenberg.words(austen_fileids[0])
-# [token for token in text if not stopwords_hash.get(token)]
-text = emma[:1000]
-list(text)
--- tests/rnn_sequence_test.py
+++ tests/rnn_sequence_test.py
@@ -6,10 +6,22 @@ X = np.array([
     [[9, 8], [6, 4], [2, 1], [8, 4]],
     [[4, 8], [2, 7], [5, 7], [6, 3]],
 ])
+
+# 위와 아래의 X는 같음
+X = np.array([[[1, 2],
+                 [4, 6],
+                 [7, 9],
+                 [5, 1]],
+
+                [[2, 1],
+                 [7, 9],
+                 [8, 9],
+                 [1, 2]]])
+
 X = X / 100
 
 y_true = np.array([1, 2, 1, 3])
 
-model = RNNTrainer(batch_size=2 , input_dim=2, hidden_dim=5, output_size=10)
-model.fit(X, y_true, learning_rate=0.05, num_epochs=100000)
+model = RNNTrainer(batch_size=2, input_dim=2, hidden_dim=5, output_size=10)
+model.fit(X, y_true, lr=0.05, n_epochs=100000)
 print()
