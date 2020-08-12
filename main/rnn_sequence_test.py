5fb2298378fa270be2508408355d30dfee62ba6f
yonghee12 <suncar1000@gmail.com>
2020-08-13 04:14:18 +0900

    testing rnnlm

--- main/rnn_sequence_test.py
+++ main/rnn_sequence_test.py
@@ -21,6 +21,6 @@ X = np.array([[[1, 2],
 X = X / 100
 
 y_true = np.array([1, 2, 1, 3])
-model = RNNTrainer(input_dim=2, hidden_dim=5, output_size=10, backend='np')
-model.fit(X, y_true, lr=0.1, n_epochs=10, batch_size=len(X))
+model = RNNTrainer(input_dim=2, hidden_dim=20, output_size=5, backend='numpy')
+model.fit(X, y_true, lr=0.2, n_epochs=1000, batch_size=1)
 print()
