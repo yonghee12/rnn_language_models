a6f247dccd066005b546f38eeb53e4454f8f997c
yonghee12 <suncar1000@gmail.com>
2020-08-12 19:00:27 +0900

    testing

--- main/rnn_sequence_test.py
+++ main/rnn_sequence_test.py
@@ -9,19 +9,18 @@ X = np.array([
 
 # 위와 아래의 X는 같음
 X = np.array([[[1, 2],
-                 [4, 6],
-                 [7, 9],
-                 [5, 1]],
+               [4, 6],
+               [7, 9],
+               [5, 1]],
 
-                [[2, 1],
-                 [7, 9],
-                 [8, 9],
-                 [1, 2]]])
+              [[2, 1],
+               [7, 9],
+               [8, 9],
+               [1, 2]]])
 
 X = X / 100
 
 y_true = np.array([1, 2, 1, 3])
-
-model = RNNTrainer(batch_size=2, input_dim=2, hidden_dim=5, output_size=10)
-model.fit(X, y_true, lr=0.05, n_epochs=100000)
+model = RNNTrainer(input_dim=2, hidden_dim=5, output_size=10, backend='np')
+model.fit(X, y_true, lr=0.1, n_epochs=10, batch_size=len(X))
 print()
--- main/rnn_test.py
+++ main/rnn_test.py
@@ -10,3 +10,16 @@ h_prev = np.random.normal(0, 1, size=(1, hidden_dim))
 model = RNNCell(1, input_dim, hidden_dim)
 model.forward(x_t, h_prev)
 model.backward(d_h_next=1)
+
+
+X = np.array([
+    [[1, 2], [4, 6], [7, 9], [5, 1]],
+    [[2, 1], [7, 9], [8, 9], [1, 2]],
+    [[9, 8], [6, 4], [2, 1], [8, 4]],
+    [[4, 8], [2, 7], [5, 7], [6, 3]],
+])
+X = X / 100
+y_true = np.array([1, 2, 1, 3])
+
+model = RNNLayer(X.shape[1], X.shape[2], hidden_dim)
+model.forward(X)
