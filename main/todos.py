211667665ce8c5a94548236cc7d5c157b516a0f5
yonghee12 <suncar1000@gmail.com>
2020-08-22 07:05:22 +0900

    added todo

---
+++ main/todos.py
@@ -0,0 +1,10 @@
+"""
+TODO
+    # deepnp
+        - reimplement: embeddings, seq2seqs
+        - integrate trainers
+        - make universal model rules
+        - find a best method for .
+    # lm
+        - train rnnlm to the last
+"""
