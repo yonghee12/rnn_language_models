dcecc70ccb19841c002ffc4f449383a319e92c25
yonghee12 <suncar1000@gmail.com>
2020-08-26 12:03:57 +0900

    checking remote deployment

--- main/seq2seq_attetnion_train.py
+++ main/seq2seq_attetnion_train.py
@@ -65,7 +65,10 @@ whole_batch = len(x_train)
 model = Seq2SeqWithAttention(embedding_dim, hidden_dim, vocab_size)
 optimizer = Adam
 trainer = Trainer(model, optimizer, lr=0.001)
+print('hello')
+print(2)
 trainer.fit(x_train, y_train, n_epochs=1, batch_size=batch_size, verbose=2)
+print()
 
 
 acc_list = []
